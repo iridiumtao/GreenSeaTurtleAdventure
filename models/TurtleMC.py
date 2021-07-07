c10d59e9175d3d4f24e4e2980598a21eaaa54a43
devilhyt <devilhyt@gmail.com>
2021-07-07 17:22:05 +0800

    fix turtle spawn position

--- models/TurtleMC.py
+++ models/TurtleMC.py
@@ -18,42 +18,46 @@ class TurtleMC(pygame.sprite.Sprite):
         del self.refImage, self.refImageWidth, self.refImageHeight
 
         self.chgImageCnter = 0
+        self.chgImageThreshold = 10
         self.imageIndex = 0
         self.images = [pygame.transform.scale((pygame.image.load("src/Turtle-0-down.png").convert_alpha()), (self.imageWidth ,self.imageHeight)),
                 pygame.transform.scale((pygame.image.load("src/Turtle-0-up.png").convert_alpha()), (self.imageWidth ,self.imageHeight))]
+        self.imageAmt = len(self.images)
         self.image = self.images[self.imageIndex]
 
-        self.rect = self.image.get_rect()
-        self.rect.x = 0
-        self.rect.y = (windowHeight + self.imageHeight)/2    
-        self.widthOffset = -50
-        self.heightOffset = 0
         self.windowWidth = windowWidth
         self.windowHeight = windowHeight
+        self.frame = 20
+        self.rect = self.image.get_rect()
+        self.rect.x = self.frame
+        self.rect.y = (windowHeight - self.imageHeight)/2    
         self.step = 10
 
     def move(self, direction):
+        # 移動
         if direction == "right":
-            if self.rect.x + self.imageWidth < self.windowWidth:
+            if self.rect.x + self.imageWidth < self.windowWidth - self.frame:
                 self.rect.x += self.step
         elif direction == "left":
-            if self.rect.x > 0 + self.widthOffset:
+            if self.rect.x > 0 + self.frame:
                 self.rect.x -= self.step
         elif direction == "up":
-            if self.rect.y > 0 + self.heightOffset:
+            if self.rect.y > 0 + self.frame:
                 self.rect.y -= self.step
         elif direction == "down":
-            if self.rect.y + self.imageHeight < self.windowHeight:
+            if self.rect.y + self.imageHeight < self.windowHeight - self.frame:
                 self.rect.y += self.step
         elif direction == "stop":
             self.rect.x = self.rect.x
             self.rect.y = self.rect.y
+        self.chgImage()
 
-        # 10 ticks換一次圖片
+    def chgImage(self):
+        # 換圖片
         self.chgImageCnter += 1
-        if self.chgImageCnter > 9:
+        if self.chgImageCnter >= self.chgImageThreshold:
             self.chgImageCnter = 0
             self.imageIndex += 1
-            if self.imageIndex > 1:
+            if self.imageIndex >= self.imageAmt:
                 self.imageIndex = 0
             self.image = self.images[self.imageIndex]
